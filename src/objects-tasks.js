596cbb828e479625f913cf3a0b94d9aad422e00a
Joelle Marianek <joelle@LAPTOP-6D50GD4A>
2024-03-10 00:33:44 +0100

    fix: linter comment in correct line

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-// eslint-disable-next-line prefer-object-spread
+
 function shallowCopy(obj) {
+  // eslint-disable-next-line prefer-object-spread
   return Object.assign({}, obj);
 }
 
